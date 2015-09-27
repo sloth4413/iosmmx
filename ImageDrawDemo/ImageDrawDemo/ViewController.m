8d2826d5ec80e260fb4050ba32a743c62792c46c
陈晓峰 <chenxiaofeng@chenxiaofengdeMacBook-Pro.local>
2015-09-27 22:06:36 +0800

    绘制平铺和拉伸

--- ImageDrawDemo/ImageDrawDemo/ViewController.m
+++ ImageDrawDemo/ImageDrawDemo/ViewController.m
@@ -9,7 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    UIImage *_centerImage;
+    
+    UIImage *_processedImage;
+}
 @end
 
 @implementation ViewController
@@ -17,6 +21,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setCenterImage];
+}
+
+-(void)setCenterImage{
+    _centerImage = [UIImage imageNamed:@"big.png"];
+    UIImageView *centerImageView = [[UIImageView alloc] initWithImage:_centerImage];
+    [self.view addSubview:centerImageView];
+    centerImageView.center = CGPointMake(CGRectGetMidX(centerImageView.superview.frame), CGRectGetMidY(centerImageView.superview.frame));
+    centerImageView.frame = CGRectIntegral(centerImageView.frame);
+    //centerImageView.contentMode = UIViewContentModeScaleToFill;
+    
+    //拉伸模式
+    UIEdgeInsets    capInsets = UIEdgeInsetsMake(_centerImage.size.height/4, _centerImage.size.width/4, _centerImage.size.height/4, _centerImage.size.width/4);
+    _processedImage = [_centerImage resizableImageWithCapInsets:capInsets resizingMode:UIImageResizingModeTile];
+    centerImageView.image =_processedImage;
+    centerImageView.bounds = CGRectMake(0, 0, _centerImage.size.width, _centerImage.size.height);
+    //[centerImageView setNeedsDisplay];
 }
 
 - (void)didReceiveMemoryWarning {
